d378d26f05a25a62f63345b50bfe2d325296462e
OrlovaKA <154547831+OrlovaKA@users.noreply.github.com>
2024-03-26 18:10:50 +0300

    Update locators.py
    Add class ProductLocators()

--- pages/locators.py
+++ pages/locators.py
@@ -7,3 +7,9 @@ class LoginPageLocators():
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
     REGISTER_FORM_EMAIL = (By.CSS_SELECTOR, "#id_registration-email")
+
+class ProductLocators():
+    BASKET_BUTTON = (By.CSS_SELECTOR, ".btn-add-to-basket")
+    PRODUCT_LABEL = (By.CSS_SELECTOR, ".alertinner > strong:nth-child(1)")
+    PRODUCT_NAME = (By.CSS_SELECTOR, ".product_main > h1")
+    SUCCESS_MESSAGE = (By.CSS_SELECTOR, "#messages > .alert-success:nth-child(1)")
